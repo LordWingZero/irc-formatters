42ef6bbaf6b2a7f5a2c62f207e47a7048f57e4c4
Victorio Berra <toryberra@gmail.com>
2016-01-11 15:43:07 -0600

    Init commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# irc-formatters
+
+Library mainly for usage by [tennu](https://github.com/Tennu/tennu) IRC bot framework.
+
+Can format text in fun ways.
+
---
+++ index.js
@@ -0,0 +1,7 @@
+var rainbow = require('./format/rainbow');
+var greentext = require('./format/greentext');
+
+module.exports = {
+    rainbow: rainbow,
+    greentext: greentext
+}
